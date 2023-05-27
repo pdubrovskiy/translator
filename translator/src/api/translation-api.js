60ab93b8e7fee67770e1733e5a0da834897a64b2
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-05-28 01:32:01 +0300

    refactor: update API because of an used limit Sun, 28 May 2023 01:31

--- translator/src/api/translation-api.js
+++ translator/src/api/translation-api.js
@@ -1,13 +1,15 @@
+import debounce from "../utils/debounce/debounce";
+
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c147d9e83emsh67a3f877dbe8350p1151e5jsn90e95c3e00af',
-		'X-RapidAPI-Host': 'wordwise3.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '9b7b08a5dbmsh724221038fc0cc2p136d71jsn07099f4ad942',
+        'X-RapidAPI-Host': 'wordwise3.p.rapidapi.com'
+    }
 };
 
 const TranslationAPI = {
-    async translate(text){
+    async translate(text) {
         const response = await fetch(`https://wordwise3.p.rapidapi.com/translate?text=${text}&dest_language=en`, options);
         const result = await response.text();
         return result.slice(1, -1);
