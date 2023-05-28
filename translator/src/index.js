4ae81c412e0fa07ca19228a3f2764ca94a377cc9
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-05-28 16:11:08 +0300

    feat: create and connect theme switcher using React contexts and local storage Sun, 28 May 2023 16:10

--- translator/src/index.js
+++ translator/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store';
+import ThemeProvider from './providers/ThemeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
   </Provider>
 );
 
--- translator/src/utils/ThemeSwitcher/ThemeSwitcher.jsx
+++ translator/src/utils/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,9 +1,19 @@
+import { ThemeContext, themes } from "../../contexts/ThemeContext"
+import classes from "./ThemeSwitcher.module.css"
+
 const ThemeSwitcher = (props) => {
-    return(
-        <div>
-            Light/Dark
+    return (
+    <ThemeContext.Consumer>
+      {({ theme, setTheme }) => (
+        <div className={classes.container}
+          onClick={() => { theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light)}}>
+          <div className={`${classes.light} ${classes.theme}`}>Light</div>
+          <div className={`${classes.dark} ${classes.theme}`}>Dark</div>
         </div>
+      )}
+    </ThemeContext.Consumer>
     );
 }
+
 
 export default ThemeSwitcher;
