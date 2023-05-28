6bee8779deda269c46a90a366e6c63900987b9be
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2023-05-28 17:13:13 +0300

    feat: add background to the application Sun, 28 May 2023 17:13

--- translator/src/index.js
+++ translator/src/index.js
@@ -11,7 +11,7 @@ root.render(
   <Provider store={store}>
     <React.StrictMode>
       <ThemeProvider>
-        <App />
+        <App className="app"/>
       </ThemeProvider>
     </React.StrictMode>
   </Provider>
